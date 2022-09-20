18768da2882b07be2fda864e9e1bb4b3cf452b6d
Bao Phan Xuan <pxbao.itus@gmail.com>
2022-09-20 15:25:26 +0700

    create ERC20 token - smart contract Swan

---
+++ sources/contracts/ERC20/Swan.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract Swan is ERC20 {
+    address payable owner;
+    constructor() ERC20 ("Swan", "SWN") {
+        _mint(msg.sender, 1000000);
+        owner = payable(msg.sender);
+        
+    }
+
+    function getTokensByEth() public payable {
+        owner.transfer(msg.value);
+        _transfer(owner, msg.sender, (msg.value /1 ether)*10);
+    }
+
+    function burnTokens(uint amount) public returns(bool) {
+        _burn(msg.sender, amount);
+        return true;
+    }
+}
