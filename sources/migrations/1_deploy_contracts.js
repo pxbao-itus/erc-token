1e9f533b8642f7dacdc04fd6f267ccdea900d166
Bao Phan Xuan <pxbao.itus@gmail.com>
2022-09-23 09:49:05 +0700

    modify for deploy 3 contract at the same time

--- sources/migrations/1_deploy_contracts.js
+++ sources/migrations/1_deploy_contracts.js
@@ -2,8 +2,7 @@ const Swan = artifacts.require("./ERC20/Swan.sol");
 const Turing = artifacts.require("./ERC721/Turing.sol");
 const Mixer = artifacts.require("./ERC1155/Mixer.sol");
 module.exports = function(deployer) {
-    // deployer.deploy(Swan);
-    // Additional contracts can be deployed here
-    // deployer.deploy(Turing);
+    deployer.deploy(Swan);
+    deployer.deploy(Turing);
     deployer.deploy(Mixer);
 };
